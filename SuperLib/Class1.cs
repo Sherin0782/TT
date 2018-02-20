5680e15d2a233c68418b68c4ad17f4fcdaceefff
Infolog <Infolog@DEVELOPMENT17>
2018-02-20 11:39:48 +0530

    Add project files.

---
+++ SuperLib/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperLib
+{
+    public class Class1
+    {
+    }
+}
